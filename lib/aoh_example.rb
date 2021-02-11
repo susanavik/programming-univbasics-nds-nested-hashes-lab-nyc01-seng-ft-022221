f81cc19be11b9ab5fe5b1301815e642d1ee43218
Susana vik <susana.m.vik@gmail.com>
2021-02-11 01:42:57 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,7 +32,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  a
+  aoh[row][key] = new_value
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
